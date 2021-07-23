0673024feacf0dc3ab013f3429fb7313acb2a9ff
skyler arnold <skyarnold1@me.com>
2021-07-22 20:40:47 -0600

    fix desearialization

--- port/src/main/java/com/blender/hub/computehub/port/manager/ManagerLinkStartResponse.java
+++ port/src/main/java/com/blender/hub/computehub/port/manager/ManagerLinkStartResponse.java
@@ -2,9 +2,11 @@ package com.blender.hub.computehub.port.manager;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ManagerLinkStartResponse {
     private String location;
 }
