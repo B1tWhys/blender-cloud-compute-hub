896d8aa7e3b79667312ad935b29ed4528f32a0e6
skyler arnold <skyarnold1@me.com>
2021-07-31 14:29:30 -0600

    add some more hmac validator tests

--- port/src/test/java/com/blender/hub/computehub/manager/HmacValidatorImplTest.java
+++ port/src/test/java/com/blender/hub/computehub/manager/HmacValidatorImplTest.java
@@ -2,6 +2,7 @@ package com.blender.hub.computehub.manager;
 
 import com.blender.hub.computehub.entity.hmac.HmacSecret;
 import com.blender.hub.computehub.port.hmac.HmacValidatorImpl;
+import com.blender.hub.computehub.usecase.hmac.port.driven.AuthenticationException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.Test;
 public class HmacValidatorImplTest {
     public static final String SECRET_ID = "9cbb53d7-7f23-4a53-8ff8-b976814fd0b5";
     public static final String SECRET_VALUE = "d091f27aaa7fb0561327b7f475e334dca112c1785d522310263c640d6042d5fa";
+    public static final String VALID_MAC = "8260e1dd246785025c80aa0ae422a5223e7d091f45adfa026dda2357206183b4";
+    public static final String VALID_MESSAGE = "http://localhost:49180/setup/link-return";
     HmacValidatorImpl validator;
     HmacSecret secret;
 
@@ -24,15 +27,31 @@ public class HmacValidatorImplTest {
     @Test
     public void validHmacTest() {
         Assertions.assertDoesNotThrow(() ->
-                validator.validate("http://localhost:49180/setup/link-return",
-                        "8260e1dd246785025c80aa0ae422a5223e7d091f45adfa026dda2357206183b4"));
+                validator.validate(VALID_MESSAGE,
+                        VALID_MAC));
     }
 
     @Test
     public void invalidHmacTest() {
-        validator = new HmacValidatorImpl(secret);
-        Assertions.assertThrows(Throwable.class, () ->
+        Assertions.assertThrows(AuthenticationException.class, () ->
                 validator.validate("foo",
-                        "8260e1dd246785025c80aa0ae422a5223e7d091f45adfa026dda2357206183b4"));
+                        VALID_MAC));
+    }
+
+    @Test
+    public void nonHexHmacValueTest() {
+        secret = HmacSecret.builder()
+                .id(SECRET_ID)
+                .value("zzzzz")
+                .build();
+        validator = new HmacValidatorImpl(secret);
+        Assertions.assertThrows(AuthenticationException.class, () ->
+                validator.validate(VALID_MESSAGE, VALID_MAC));
+    }
+
+    @Test
+    public void nonHexMacTest() {
+        Assertions.assertThrows(AuthenticationException.class, () ->
+                validator.validate(VALID_MESSAGE, "ghijklmnop"));
     }
 }
